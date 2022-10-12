e1b23979fddad0b37af6953c8fe51b7d678fcc78
Chung10Kr <coohg93@hanmail.net>
2022-10-12 23:35:37 +0900

    Socket.js 코드 remove

--- slack/lib/socket.js
+++ slack/lib/socket.js
@@ -1,4 +1,3 @@
-const { App } = require('@slack/bolt');
 
 const config = require("../../config");
 
@@ -10,34 +9,10 @@ class Socket{
     }
     _setToken()
     {
-
-      this.app = new App({
-        token: config.botToken,
-        signingSecret: config.signingSecret,
-        socketMode : true,
-        appToken : config.appToken,
-        port : 3000
-      });
-
     }
 
     async connect()
     {
-      let self = this;
-      // Listens to incoming messages that contain "hello"
-      this.app.message('123', async ({ message, say }) => {
-        console.log("!!!!!!!!!!!")
-        // say() sends a message to the channel where the event was triggered
-        await say(`Hey there <@${message.user}>!`);
-      });
-
-      
-      (async () => {
-        // Start your app
-        let oResult = await self.app.start();
-        console.log( oResult )
-        console.log('⚡️ Bolt app is running!');
-      })();
     }
     
 }
